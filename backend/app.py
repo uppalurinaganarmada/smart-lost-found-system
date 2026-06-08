1d27ebf205764202faf9922b6fd2d0d2b404083c
Naga Narmada Uppaluri <uppalurinaganarmada@gmail.com>
2026-06-08 22:32:31 +0530

    Completed Phase 2 and connected pages

--- backend/app.py
+++ backend/app.py
@@ -5,6 +5,17 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
+@app.route('/report-lost')
+def report_lost():
+    return render_template('report_lost.html')
+
+@app.route('/report-found')
+def report_found():
+    return render_template('report_found.html')
+
+@app.route('/search')
+def search():
+    return render_template('search.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
